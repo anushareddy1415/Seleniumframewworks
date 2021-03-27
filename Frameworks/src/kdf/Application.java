8dbc45c0e90032e9c56f436d7f5b28b20c593f80
anush <anush@192.168.0.70>
2021-03-26 21:15:02 -0400

    coding changes

--- Frameworks/src/kdf/Application.java
+++ Frameworks/src/kdf/Application.java
@@ -13,6 +13,7 @@ public class Application {
 	public void verifyInvalidLogin() throws IOException {
 		String[][] data = GenericMethods.getData("C:\\Anu-BA\\testing\\Automation\\class 9\\TestData.xlsx", "Sheet2");
 	Methods mtd = new Methods();
+	//Making changes to code
 		for(int i = 1;i<data.length;i++) {
 	switch(data[i][3]) {
 	    case "openBrowser":
